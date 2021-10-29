dc16d8e4a42292bb265c0411e874cb9257009540
Alex Baumgertner <alex.baumgertner@gmail.com>
2021-10-29 22:16:01 +0300

    Init logux
    https://logux.io/guide/starting/new-redux-client/

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { CrossTabClient,  badge, badgeEn, log } from '@logux/client'
+import { badgeStyles } from '@logux/client/badge/styles'
+import { createStoreCreator } from '@logux/redux'
+import { Provider } from 'react-redux'
+
+import reducer from './reducers'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './index.css';
+
+const client = new CrossTabClient({
+  server: process.env.NODE_ENV === 'development'
+    ? 'ws://localhost:31337'
+    : 'wss://logux.example.com',
+  subprotocol: '1.0.0',
+  userId: 'anonymous',  // TODO: We will fill it in Authentication recipe
+  token: ''  // TODO: We will fill it in Authentication recipe
+})
+
+const createStore = createStoreCreator(client)
+const store = createStore(reducer)
+
+badge(store.client, { messages: badgeEn, styles: badgeStyles })
+log(store.client)
+store.client.start()
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}><App /></Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
