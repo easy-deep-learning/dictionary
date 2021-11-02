e0455ff4539f37a12c6a6f1eeb35e3df80f50e85
Alex Baumgertner <alex.baumgertner@gmail.com>
2021-11-02 19:29:16 +0300

    Fix index page result

--- client-server-express/src/index.js
+++ client-server-express/src/index.js
@@ -34,7 +34,7 @@ app.use(auth(config))
 // req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
   // res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
-  res.render('pages/index')
+  res.render('pages/index', {result: null})
 })
 
 app.get('/profile', requiresAuth(), (req, res) => {
